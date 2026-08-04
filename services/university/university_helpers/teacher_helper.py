68208544a54fd3565277ace08fb1e378c6d7848e
Konstantinv <yetifor@mail.ru>
2026-08-04 13:58:25 +0300

    add tests

--- services/university/university_helpers/teacher_helper.py
+++ services/university/university_helpers/teacher_helper.py
@@ -6,6 +6,11 @@ class TeacherHelper(BaseHelper):
 
     ROOT_ENDPOINT = f"{END_PREFIX}/"
 
+    TEACHER_ID_ENDPOINT = f"{END_PREFIX}{{TeacherId}}"
     def post_teacher(self, json: dict) -> requests.Response:
         response = self.api_utils.post(self.ROOT_ENDPOINT, json=json)
+        return response
+
+    def delete_teacher(self, teacher_id: int) -> requests.Response:
+        response = self.api_utils.delete(f"{self.ROOT_ENDPOINT}{teacher_id}")
         return response
---
+++ tests/test_teacher_contract.py
@@ -0,0 +1,43 @@
+from services.university.university_helpers.teacher_helper import TeacherHelper
+from services.university.university_models.base_teacher import SubjectEnum
+from services.university.university_models.teacher_request import TeacherRequest
+from faker import Faker
+faker = Faker()
+import random
+from logger.logger import Logger
+
+class TestTeacherContract:
+    def test_create_teacher_admin(self, university_api_utils_admin):
+        Logger.info(f"### Step 1. Create teacher")
+        teacher_helper = TeacherHelper(api_utils=university_api_utils_admin)
+        teacher = teacher_helper.post_teacher({'first_name':faker.first_name(),
+                                               'last_name':faker.last_name(),
+                                               'subject': random.choice([option for option in SubjectEnum])})
+        assert teacher.status_code == 201, \
+            (f"Wrong status code: {teacher.status_code}",
+             f"Assert status code: 201",)
+
+    def test_create_teacher_anonym(self, university_api_utils_anonym):
+        Logger.info(f"### Step 1. Create teacher")
+        teacher_helper = TeacherHelper(api_utils=university_api_utils_anonym)
+        teacher = teacher_helper.post_teacher({'first_name': faker.first_name(),
+                                               'last_name': faker.last_name(),
+                                               'subject': random.choice([option for option in SubjectEnum])})
+        assert teacher.status_code == 403, \
+            (f"Wrong status code: {teacher.status_code}",
+             f"Assert status code: 403",)
+
+    def test_teacher_delete(self, university_api_utils_admin):
+        Logger.info(f"### Step 1. Create teacher")
+        teacher_helper = TeacherHelper(api_utils=university_api_utils_admin)
+        teacher = teacher_helper.post_teacher({'first_name': faker.first_name(),
+                                               'last_name': faker.last_name(),
+                                               'subject': random.choice([option for option in SubjectEnum])})
+
+        teacher_id = teacher_id = teacher.json()['id']
+        Logger.info(f"### Step 2. Delete teacher")
+        teacher_delete = teacher_helper.delete_teacher(teacher_id)
+
+
+        assert teacher_delete.status_code == 200, (f"Wrong status code: {teacher_delete.status_code}",)
+
